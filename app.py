ba8dc559b698e4d30cb80eb48cc9555f397532f4
syukriyansyah-ipb <2020syukriyansyah@apps.ipb.ac.id>
2024-12-09 16:49:01 +0700

    change layout

--- app.py
+++ app.py
@@ -20,30 +20,31 @@ unique_counts.columns = ["Kelas", "Jumlah"]
 data_pkm = pd.read_excel('daftar_pkm_2025.xlsx')
 
 st.header("STATISTIK PKM SISFOR 2025")
-st.subheader('Daftar PKM')
-st.dataframe(data_pkm[['Bidang', 'Kelas', 'Pembimbing']], use_container_width=True)
+
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Kehadiran Sosialisasi")
-    st.bar_chart(unique_counts.set_index("Kelas"))
+    # st.subheader("Kehadiran Sosialisasi")
+    # st.bar_chart(unique_counts.set_index("Kelas"))
+    st.subheader('Daftar PKM')
+    st.dataframe(data_pkm[['Bidang', 'Kelas', 'Pembimbing']], use_container_width=True)
     
 with col2:
-    st.header("Jumlah PKM Per Kelas")
+    st.subheader("Jumlah PKM Per Kelas")
     
     class_unique_counts = data_pkm["Kelas"].value_counts().reset_index()
     class_unique_counts.columns = ["Kelas", "Jumlah"]
     st.bar_chart(class_unique_counts.set_index("Kelas"))
 
 with col1:
-    st.header("Dosen Pendamping / PKM")
+    st.subheader("Dosen Pendamping / PKM")
     dosen_unique_counts = data_pkm["Pembimbing"].value_counts().reset_index()
     dosen_unique_counts.columns = ["Pembimbing", "Jumlah"]
     st.bar_chart(dosen_unique_counts.set_index("Pembimbing"))
     
 with col2:
-    st.header("Bidang PKM")
+    st.subheader("Bidang PKM")
     bidang_unique_counts = data_pkm["Bidang"].value_counts().reset_index()
     bidang_unique_counts.columns = ["Bidang", "Jumlah"]
     st.bar_chart(bidang_unique_counts.set_index("Bidang"))
